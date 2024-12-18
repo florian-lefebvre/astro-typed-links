a51742b86abf90d4132ab2e6e308aeea94b94fe1
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-12-18 10:56:44 +0100

    fix: nodefs leak (#5)

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,4 +1,13 @@
 import { integration } from "./integration.js";
-export * from "./link.js";
+import { link as _link } from "./link.js";
+
+/**
+ * @deprecated Use `link` exported from `astro-typed-links/link` instead. Importing this
+ * function client-side will cause the build to fail. It will be removed in the next major
+ */
+export const link = _link;
 
 export default integration;
+
+// biome-ignore lint/suspicious/noEmptyInterface: used for augmentation
+export interface AstroTypedLinks {}
